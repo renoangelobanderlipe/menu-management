6f595e666c1194fbcd8bbc541f25485d909a17d9
Reno Angelo Banderlipe <personal.renoangelo@gmail.com>
2024-06-14 10:38:59 +0800

    added screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
+  theme: {
+    screens: {
+      sm: "640px", // => @media (min-width: 640px) { ... }
+      md: "768px", // => @media (min-width: 768px) { ... }
+      lg: "1024px", // => @media (min-width: 1024px) { ... }
+      xl: "1280px", // => @media (min-width: 1280px) { ... }
+      "2xl": "1536px", // => @media (min-width: 1536px) { ... }
+    },
+
+  },
 
   plugins: [],
 });
