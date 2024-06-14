7584e0c349b603fe4983945dc054ab214aabf943
Reno Angelo Banderlipe <personal.renoangelo@gmail.com>
2024-06-14 22:03:51 +0800

    added theme

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "@material-tailwind/react";
 import { Toaster } from "sonner";
+import { theme } from "@services/provider/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
+    <ThemeProvider value={theme}>
       <App />
-      <Toaster richColors expand  position="top-right" />
+      <Toaster richColors expand position="top-right" />
     </ThemeProvider>
   </StrictMode>
 );
