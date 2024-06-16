97fad1b3e20cfdfba00aa50c66fe8478ab994e2d
Reno Angelo Banderlipe <personal.renoangelo@gmail.com>
2024-06-16 10:55:26 +0800

    refine zod validations with prettier formatted code

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,55 +1,54 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const loginSchema = z.object({
   email: z.string().email(),
   password: z.string(),
 });
 
-
 const menuItemSchema = z.object({
   image: z
     .any()
-    .refine((file) => {
-      if (file instanceof File) {
-        return (
-          file.type === "image/jpeg" ||
-          file.type === "image/png" ||
-          file.type === "image/svg+xml"
-        );
-      } else if (file) {
-        return false;
-      }
-      return true;
-    }, { message: "Invalid file type. Please upload JPG, PNG, or SVG." })
+    .refine(
+      (file) => {
+        if (file instanceof File) {
+          return file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/svg+xml';
+        } else if (file) {
+          return false;
+        }
+        return true;
+      },
+      { message: 'Invalid file type. Please upload JPG, PNG, or SVG.' },
+    )
     .optional(),
-  itemName: z
-    .string()
-    .min(1, "Item name is required"),
+  itemName: z.string().min(1, 'Item name is required'),
   category: z
     .string()
-    .min(1, "At least one category is required")
-    .transform((value) => value.split(",").map((cat) => cat.trim()))
-    .refine((categories) => categories.length <= 3, {
-      message: "Maximum of 3 categories allowed.",
-    }),
+    .min(1, 'At least one category is required')
+    .refine(
+      (val) =>
+        !val ||
+        val.split(',').map((categories) => categories.length <= 3, {
+          message: 'Maximum of 3 categories allowed.',
+        }),
+    ),
   price: z.preprocess(
-    (arg) => (typeof arg == "string" ? parseFloat(arg) : arg),
-    z.number().min(0, "Price must be a positive number")
+    (arg) => (typeof arg == 'string' ? parseFloat(arg) : arg),
+    z.number().min(0, 'Price must be a positive number'),
   ),
   cost: z.preprocess(
-    (arg) => (typeof arg == "string" ? parseFloat(arg) : arg),
-    z.number().min(0, "Cost must be a number")
+    (arg) => (typeof arg == 'string' ? parseFloat(arg) : arg),
+    z.number().min(0, 'Cost must be a number'),
   ),
   amountInStock: z.preprocess(
-    (arg) => (typeof arg == "string" ? parseInt(arg) : arg),
-    z.number().min(0, "Amount in stock must be a positive number")
+    (arg) => (typeof arg == 'string' ? parseInt(arg) : arg),
+    z.number().min(0, 'Amount in stock must be a positive number'),
   ),
   options: z
     .string()
     .optional()
-    .refine((val) => !val || val.split(",").map((o) => o.trim()).length <= 4, {
-      message: "You can add up to 4 options",
+    .refine((val) => !val || val.split(',').map((options) => options.trim()).length <= 4, {
+      message: 'You can add up to 4 options',
     }),
 });
 
-export {loginSchema, menuItemSchema}
+export { loginSchema, menuItemSchema };
