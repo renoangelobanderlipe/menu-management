37a191ed111b98f5c8f59983d2a8f3408d4079f4
Reno Angelo Banderlipe <personal.renoangelo@gmail.com>
2024-06-16 15:16:01 +0800

    affected by global formatter

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,4 @@
 const searchFields = ['itemName', 'itemDescription', 'category'];
 const tableHead = ['Item Name', 'Category', 'Options Available', 'Amount In Stock', 'Price', 'Cost', 'Action'];
 
-export {
-  searchFields,
-  tableHead
-}
+export { searchFields, tableHead };
