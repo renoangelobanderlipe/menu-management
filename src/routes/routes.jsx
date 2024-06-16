405061860d83aa4b61a51d50cb3844ceded5ef1b
Reno Angelo Banderlipe <personal.renoangelo@gmail.com>
2024-06-17 00:20:16 +0800

    tried removing vercel config

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,41 +6,20 @@ import NotFoundComponent from '@components/ui/NotFoundComponent';
 const LoginPage = lazy(() => import('@pages/en/Auth/LoginPage'));
 const MenuManagementPage = lazy(() => import('@pages/en/MenuManagement/MenuManagementPage'));
 
-// export const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: (
-//       <Suspense fallback={<div>Loading...</div>}>
-//         <MenuManagementPage />
-//       </Suspense>
-//     ),
-//     errorElement: <NotFoundComponent />,
-//   },
-//   {
-//     path: '/login',
-//     element: <LoginPage />,
-//     errorElement: <NotFoundComponent />,
-//   },
-//   {
-//     path: '*',
-//     element: <NotFoundComponent />,
-//   },
-// ]);
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Suspense fallback={<div>Loading...</div>}><NotFoundComponent /></Suspense>,
+    element: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <MenuManagementPage />
+      </Suspense>
+    ),
     errorElement: <NotFoundComponent />,
-    children: [
-      {
-        index: true,
-        element: <Suspense fallback={<div>Loading...</div>}><MenuManagementPage /></Suspense>,
-      },
-      {
-        path: 'login',
-        element: <Suspense fallback={<div>Loading...</div>}><LoginPage /></Suspense>,
-      },
-    ],
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+    errorElement: <NotFoundComponent />,
   },
   {
     path: '*',
