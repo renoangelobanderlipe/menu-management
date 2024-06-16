c1b69eb0ee92f73aff5ee50069c6844872ebcb28
Reno Angelo Banderlipe <personal.renoangelo@gmail.com>
2024-06-16 17:27:42 +0800

    move toaster to bottom right position

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider value={theme}>
       <App />
-      <Toaster richColors expand position="top-right" closeButton invert />
+      <Toaster richColors expand position="bottom-right" closeButton invert />
     </ThemeProvider>
   </StrictMode>,
 );
